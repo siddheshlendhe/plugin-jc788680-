6641c97b92cf6a4a0caed1bfe2ad51cc97f1f5af
Siddhesh <siddheshlendhe35@gmail.com>
2021-01-19 08:02:28 +1000

    Added footer in backend page

--- pricing_tables/prricing_table.php
+++ pricing_tables/prricing_table.php
@@ -65,7 +65,7 @@ if( ! defined( 'PTP_PLUGIN_PATH' ) ) {
   function dh_ptp_plugin_footer ($text) {
     echo
   	$text . ' '.
-  	sprintf( __('Thank you for using Pricing Tables.') ). ' ' .
+  	sprintf( __('Thank you for using Easy Pricing Tables.') ). ' ' .
   	sprintf( __('Copyright reserved by Siddhesh Lendhe')); 
   }
 
@@ -81,8 +81,59 @@ if( ! defined( 'PTP_PLUGIN_PATH' ) ) {
     }
   }
   add_action('admin_enqueue_scripts', 'dh_ptp_plugin_footer_enqueue');
-  	
+
+  /* Localization */
+  function fca_eoi_load_localization_easy_pricing_tables() {
 	
+    $locale = apply_filters( 'plugin_locale', get_locale(), 'easy-pricing-tables' );
+    
+    load_textdomain( 'easy-pricing-tables', trailingslashit( WP_LANG_DIR ) . 'easy-pricing-tables' . '/' . 'easy-pricing-tables' . '-' . $locale . '.mo' );
+  
+	load_plugin_textdomain( 'easy-pricing-tables', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+  }
+  add_action( 'plugins_loaded', 'fca_eoi_load_localization_easy_pricing_tables' );
+  	
+	//DEACTIVATION SURVEY 
+	function fca_ptp_admin_deactivation_survey( $hook ) {
+		if ( $hook === 'plugins.php' ) {
+			
+			ob_start(); ?>
+			
+			<div id="fca-deactivate" style="position: fixed; left: 232px; top: 191px; border: 1px solid #979797; background-color: white; z-index: 9999; padding: 12px; max-width: 669px;">
+				<h3 style="font-size: 14px; border-bottom: 1px solid #979797; padding-bottom: 8px; margin-top: 0;"><?php _e( 'Sorry to see you go', 'landing-page-cat' ) ?></h3>
+				<p><?php _e( 'Hi, this is David, the creator of Easy Pricing Tables. Thanks so much for giving my plugin a try. I’m sorry that you didn’t love it.', 'landing-page-cat' ) ?>
+				</p>
+				<p><?php _e( 'I have a quick question that I hope you’ll answer to help us make Easy Pricing Tables better: what made you deactivate?', 'landing-page-cat' ) ?>
+				</p>
+				<p><?php _e( 'You can leave me a message below. I’d really appreciate it.', 'landing-page-cat' ) ?>
+				</p>
+				
+				<p><textarea style='width: 100%;' id='fca-ept-deactivate-textarea' placeholder='<?php _e( 'What made you deactivate?', 'landing-page-cat' ) ?>'></textarea></p>
+				
+				<div style='float: right;' id='fca-deactivate-nav'>
+					<button style='margin-right: 5px;' type='button' class='button button-secondary' id='fca-ept-deactivate-skip'><?php _e( 'Skip', 'landing-page-cat' ) ?></button>
+					<button type='button' class='button button-primary' id='fca-ept-deactivate-send'><?php _e( 'Send Feedback', 'landing-page-cat' ) ?></button>
+				</div>
+			
+			</div>
+			
+			<?php
+				
+			$html = ob_get_clean();
+			
+			$data = array(
+				'html' => $html,
+				'nonce' => wp_create_nonce( 'fca_ptp_uninstall_nonce' ),
+				'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			);
+						
+			wp_enqueue_script('fca_ptp_deactivation_js', plugins_url( '', __FILE__ ) . '/includes/deactivation.min.js' );
+			wp_localize_script( 'fca_ptp_deactivation_js', "fca_ptp", $data );
+		}
+		
+		
+	}	
+	add_action( 'admin_enqueue_scripts', 'fca_ptp_admin_deactivation_survey' );
 }
 
 //UNINSTALL ENDPOINT
